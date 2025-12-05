8a7c4be20002d91761ade450e8b6b98a96e2730d
sowmisasi0609 <sowmisasi0609@gmail.com>
2025-12-05 16:27:00 +0530

    update requirements with gunicorn

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = 'django-insecure-l&71(2xn(b6rk*gt$l8j(xv9*3!6dds+)(4bfv@q==w75wx&k_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -106,7 +106,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
